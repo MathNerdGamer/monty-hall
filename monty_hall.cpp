95935e866c3a62007ce199ba4dcc1e9f14f264e6
Math Nerd <34165643+MathNerdGamer@users.noreply.github.com>
2024-02-08 16:57:08 -0600

    Update monty_hall.cpp
    Edited code to make it cleaner using an enum class.

--- monty_hall.cpp
+++ monty_hall.cpp
@@ -3,22 +3,15 @@
 #include <sstream>
 #include <vector>
 
-namespace math_nerd
+enum class prize
 {
-    struct prize
-    {
-        enum
-        {
-            goat,
-            car
-        };
-    };
-}
+    goat,
+    car
+};
 
 using namespace std;
-using namespace math_nerd;
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
     int total_sims{ 1000 };
     constexpr int doors{ 3 };
@@ -26,18 +19,18 @@ int main(int argc, char **argv)
     {   // Scope for stringstream.
         stringstream buf;
 
-        switch( argc )
+        switch ( argc )
         {
             case 2:
             {
                 buf << argv[1];
                 buf >> total_sims;
-                
-                if( total_sims > 1000000000 )
+
+                if ( total_sims > 1000000000 )
                 {
                     total_sims = 1000000000;
                 }
-                else if( total_sims < 10000 )
+                else if ( total_sims < 10000 )
                 {
                     total_sims = 10000;
                 }
@@ -56,28 +49,24 @@ int main(int argc, char **argv)
 
     mt19937 rng{ seed() };
 
-    uniform_int_distribution<int> select_door(0, 2);
+    uniform_int_distribution<int> select_door( 0, 2 );
 
     int successes{ 0 };
 
     long double win_pct{ 0.0 };
 
-    for( auto sim{ 0 }; sim < total_sims; ++sim )
+    for ( auto sim{ 0 }; sim < total_sims; ++sim )
     {
         // Set up prizes
-        vector<int> door(doors, 0);
-        for( auto it{ 0 }; it < doors; ++it )
-        {
-            door[it] = prize::goat;
-        }
-        door[select_door(rng)] = prize::car;
+        vector<prize> door( doors, prize::goat );
+        door[select_door( rng )] = prize::car;
 
         // Contestant selects a door
-        auto contestant = select_door(rng);
+        auto contestant = select_door( rng );
 
         // Here, Monty Hall opens all doors besides the door the contestant chose and one other.
         // This other door must be the car if the contestant's selection was a goat. Therefore,
-        if( door[contestant] == prize::goat )
+        if ( door[contestant] == prize::goat )
         {
             ++successes;
         }
@@ -87,7 +76,7 @@ int main(int argc, char **argv)
 
     std::cout << "Result: " << successes << " / " << total_sims << '\n';
 
-    std::cout << "Using the optimal strategy, to swap after a goat is revealed, one wins the car " << 100*win_pct << "% of the time.\n";
+    std::cout << "Using the optimal strategy, to swap after a goat is revealed, one wins the car " << 100 * win_pct << "% of the time.\n";
 
     return EXIT_SUCCESS;
 }
